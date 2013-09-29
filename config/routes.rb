cc22673988caaf10e529ffb9636c17c5f9038030
Kai <Kgr78@web.de>
2013-09-29 09:40:44 +0200

    Message

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
-
-  get "static_pages/about"
-
+  
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match 'static_pages/home',    to: 'static_pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
