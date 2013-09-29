2e5be40774cf3e6ba614e0ca0e2427f7e9f85362
Kai <Kgr78@web.de>
2013-09-29 10:11:27 +0200

    Routes and Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
-  
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
-  match 'static_pages/home',    to: 'static_pages#home'
+  get "users/new"
+
+root to: 'static_pages#home'
+match '/help', to: 'static_pages#help'
+match '/about', to: 'static_pages#about'
+match '/contact', to: 'static_pages#contact'
+match '/static_pages/home', :to =>'static_pages#home'
+match '/signup',  to: 'users#new',            via: 'get'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
